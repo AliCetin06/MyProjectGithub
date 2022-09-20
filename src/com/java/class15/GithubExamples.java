29607479abb5669298daf0d11ed991158ffa6227
Your Name <ali.ubeyd.cetin@gmail.com>
2022-09-20 11:45:37 -0400

    creating class15 and a new class

---
+++ src/com/java/class15/GithubExamples.java
@@ -0,0 +1,11 @@
+package com.java.class15;
+
+public class GithubExamples {
+
+	public static void main(String[] args) {
+		System.out.println("This is from Ali");
+		System.out.println("My task is completed for today");
+
+	}
+
+}
